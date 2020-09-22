a7db13a80fe01d08e6b3b4ee63d71bd9c6f0ce9a
Dieter Holvoet <dieter.holvoet@gmail.com>
2020-09-22 15:38:23 +0200

    Remove usage of PHP 7.3 syntax

--- src/Service/Snapshot/HtmlRouteProvider.php
+++ src/Service/Snapshot/HtmlRouteProvider.php
@@ -26,7 +26,7 @@ class HtmlRouteProvider extends AdminHtmlRouteProvider
                 [],
                 [
                     '_permission' => 'administer wmcontent',
-                ],
+                ]
             )
         );
 
